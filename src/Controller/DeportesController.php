e7afb24696a582e7aa8d876cec63553050ef87da
Websdirect <luis.prieto@websdirect.es>
2019-02-21 15:27:20 +0100

    Paso 4 Ejercicio 1

--- src/Controller/DeportesController.php
+++ src/Controller/DeportesController.php
@@ -2,9 +2,58 @@
 namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
 
-class DeportesController
+class DeportesController extends Controller
 {
+
+    /**
+     * @Route("/deportes/usuario", name="usuario" )
+     */
+    public function sesionUsuario(Request $request) {
+        $usuario_get=$request->query->get('nombre');
+        $session = $request->getSession();
+        $session->set('nombre', $usuario_get);
+        return $this->redirectToRoute('usuario_session',array('nombre'=>$usuario_get));
+    }
+
+    /**
+     * @Route("/deportes/usuario/{nombre}", name="usuario_session" )
+    */
+    public function paginaUsuario() {
+        $session=new Session();
+        $usuario=$session->get('nombre');
+        return new Response(sprintf('Sesión iniciada con el atributo nombre: %s', $usuario
+        ));
+    }
+
+    /**
+     * @Route(
+     *     "/deportes/{_locale}/{fecha}/{seccion}/{equipo}/{slug}.{_format}",
+     *     defaults={"slug": "1","_format":"html"},
+     *     requirements={
+     *         "_locale": "es|en",
+     *         "_format": "html|json|xml",
+     *          "fecha": "[\d+]{8}"
+     *     }
+     * )
+     */
+    public function rutaAvanzada($_locale,$fecha, $seccion, $equipo, $slug) {
+        // Simulamos una base de datos de equipos o personas
+        $sports=["valencia", "barcelona","federer", "rafa-nadal"];
+        // Si el equipo o persona que buscamos no se encuentra redirigimos
+        // al usuario a la página de inicio
+        if(!in_array($equipo,$sports)) {
+            return $this->redirectToRoute('lista_paginas');
+        }
+        return new Response(sprintf(
+            'Mi noticia en idioma=%s,
+      fecha=%s,deporte=%s,equipo=%s, noticia=%s ',
+            $_locale, $fecha, $seccion, $equipo, $slug));
+    }
+
     /**
      * @Route(
      *     "/deportes/{_locale}/{fecha}/{seccion}/{equipo}/{pagina}",
@@ -24,24 +73,6 @@ class DeportesController
             $_locale, $fecha, $seccion, $equipo, $pagina));
     }
 
-    /**
-     * @Route(
-     *    "/deportes/{_locale}/{fecha}/{seccion}/{equipo}/{slug}.{_format}",
-     *     defaults={"slug": "1","_format":"html"},
-     *     requirements={
-     *         "_locale": "es|en",
-     *         "_format": "html|json|xml",
-     *          "fecha": "[\d+]{8}"
-     *     }
-     * )
-     */
-    public function rutaAvanzada($_locale,$fecha, $seccion, $equipo, $slug) {
-        return new Response(sprintf(
-            'Mi noticia en idioma=%s,
-      fecha=%s,deporte=%s,equipo=%s, noticia=%s ',
-            $_locale, $fecha, $seccion, $equipo, $slug));
-    }
-
 
 
     /**
@@ -49,10 +80,16 @@ class DeportesController
      *      requirements={"pagina"="\d+"},
      *      defaults={"seccion":"tenis"})
      */
-    public function lista($seccion,$pagina=1) {
-        return new Response(sprintf(
-            'Deportes seccion: seccion %s, listado de noticias página %s',
-            $seccion, $pagina));
+    public function lista($seccion, $pagina=1) {
+        // Simulamos una base de datos de deportes
+        $sports=["futbol", "tenis","rugby"];
+        // Si el deporte que buscamos no se encuentra lanzamos la
+        // excepcion 404 deporte no encontrado
+        if(!in_array($seccion,$sports)) {
+            throw $this->createNotFoundException('Error 404 este deporte no está en nuestra Base de Datos');
+        }
+        return new Response(sprintf( 'Deportes seccion: seccion %s, listado de noticias página %s', $seccion, $pagina));
+
     }
     /**
      * @Route("/deportes/{seccion}/{slug} ",
